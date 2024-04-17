2674579c22fc4636de8e85c5bcd1d5f2bb37d313
jordydev1993 <166775981+jordydev1993@users.noreply.github.com>
2024-04-17 11:52:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 // init AOS animation
-AOS.init({
-    duration: 1000,
-    offset: 100,
-});
+//AOS.init({
+//    duration: 1000,
+//    offset: 100,
+//});
 
 const trendingSushis = [
     'Make Sushi',
